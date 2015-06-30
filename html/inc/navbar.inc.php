46859c01a8a9071d3b01bb594049342dec66ebe4
Todd Swatling <todd.swatling@gmail.com>
2015-06-30 16:37:22 -0400

    Navbar now shows number of total patches, broken down by urgency, and number of servers needing patches.

--- html/inc/navbar.inc.php
+++ html/inc/navbar.inc.php
@@ -11,6 +11,62 @@ mysql_select_db(DB_NAME,$patch_list_link);
 $patch_list_res = mysql_query($patch_list_sql);
 $patch_list_row = mysql_fetch_array($patch_list_res);
 $patches_to_apply_count = $patch_list_row['total_found'];
+
+//Count number of high urgency security patches
+$high_urg_sql = "SELECT server_name, package_name, urgency FROM patches WHERE urgency = 'high' OR urgency = 'Critical' OR urgency = 'Important' OR urgency = 'emergency';";
+$high_urg_res = mysql_query($high_urg_sql);
+while($row = mysql_fetch_array($high_urg_res)){
+	$high_urg_arr_name[] = $row['server_name'];
+	$high_urg_arr[] = $row['package_name'];
+}
+$high_urg_count = $high_urg = $high_urg_pct = 0;
+if ( !empty($high_urg_arr_name) && !empty($high_urg_arr) ){
+	$high_urg = count(array_unique($high_urg_arr_name));
+	$high_urg_count = count($high_urg_arr);
+	$high_urg_pct =  ( $high_urg_count / $patches_to_apply_count ) * 100;
+}
+//Count number of medium urgency security patches
+$med_urg_sql = "SELECT server_name, package_name FROM patches WHERE urgency = 'medium' OR urgency = 'Moderate';";
+$med_urg_res = mysql_query($med_urg_sql);
+while($row = mysql_fetch_array($med_urg_res)){
+	$med_urg_arr_name[] = $row['server_name'];
+	$med_urg_arr[] = $row['package_name'];
+}
+$med_urg_count = $med_urg = $med_urg_pct = 0;
+if ( !empty($med_urg_arr_name) && !empty($med_urg_arr) ){
+	$med_urg = count(array_unique($med_urg_arr_name));
+	$med_urg_count = count($med_urg_arr);
+	$med_urg_pct = ( $med_urg_count / $patches_to_apply_count ) * 100;
+}
+
+//Count number of low urgency security patches
+$low_urg_sql = "SELECT server_name, package_name FROM patches WHERE urgency = 'low' OR urgency = 'Low';";
+$low_urg_res = mysql_query($low_urg_sql);
+while($row = mysql_fetch_array($low_urg_res)){
+	$low_urg_arr_name[] = $row['server_name'];
+	$low_urg_arr[] = $row['package_name'];
+}
+$low_urg_count = $low_urg = $low_urg_pct = 0;
+if ( !empty($low_urg_arr_name) && !empty($low_urg_arr) ){
+	$low_urg = count(array_unique($low_urg_arr_name));
+	$low_urg_count = count($low_urg_arr);
+	$low_urg_pct = ( $low_urg_count / $patches_to_apply_count ) * 100;
+}
+
+//Count number of bugfix and enhancement patches
+$bug_urg_sql = "SELECT server_name, package_name FROM patches WHERE urgency = 'bugfix' OR urgency = 'enhancement';";
+$bug_urg_res = mysql_query($bug_urg_sql);
+while($row = mysql_fetch_array($bug_urg_res)){
+	$bug_urg_arr_name[] = $row['server_name'];
+	$bug_urg_arr[] = $row['package_name'];
+}
+$bug_urg_count = $bug_urg = $bug_urg_pct = 0;
+if ( !empty($bug_urg_arr_name) && !empty($bug_urg_arr) ){
+	$bug_urg = count(array_unique($bug_urg_arr_name));
+	$bug_urg_count = count($bug_urg_arr);
+	$bug_urg_pct = ( $bug_urg_count / $patches_to_apply_count ) * 100;
+}
+
 mysql_close($patch_list_link);
 $data = "";
 foreach ($navbar_array as $key=>$val){
@@ -43,7 +99,21 @@ foreach ($navbar_array as $key=>$val){
          * TODO: work the badge code in dynamically with patche count
          */
         if ($page_string == "patches"){
-            $badge_code = "&nbsp;&nbsp;<span class=\"badge\">$patches_to_apply_count</span>";
+            $badge_code = "&nbsp;&nbsp;<span class=\"label label-default\">$patches_to_apply_count Total</span> &nbsp;&nbsp; 
+				<br><br>
+                <div class=\"progress\">
+				  <div class=\"progress-bar progress-bar-primary\" style=\"width: $bug_urg_pct%\">$bug_urg_count Bug</div>
+				  <div class=\"progress-bar progress-bar-info\" style=\"width: $low_urg_pct%\">$low_urg_count Low</div>
+  				  <div class=\"progress-bar progress-bar-warning\" style=\"width: $med_urg_pct%\">$med_urg_count Med</div>
+				  <div class=\"progress-bar progress-bar-danger\" style=\"width: $high_urg_pct%\">$high_urg_count High</div>
+				</div>
+				Servers:
+				<br>
+                <span class=\"label label-primary\">$bug_urg Bug</span>
+                <span class=\"label label-info\">$low_urg Low</span>
+                <span class=\"label label-warning\">$med_urg Med</span> 
+                <span class=\"label label-danger\">$high_urg High</span>  
+                ";
         }
         else{
             $badge_code = "";
